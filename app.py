d8a0fb0268fd1d069ce6101e120045d94f1f312b
imthaghost <imthaghost@protonmail.com>
2019-09-26 03:43:49 -0700

    [add] some cool stuff

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import sys
 
 
 try:
-    from flask import Flask, render_template, redirect, request
+    from flask import Flask, render_template, redirect, request, url_for
     import requests
+    from pymongo import MongoClient
+    from bson.objectid import ObjectId
 except (ImportError, ModuleNotFoundError) as e:
     print(e, file=sys.stderr)
-    print("run pipenv install 'moduleName' in your terminal ")
+    print('\x1b[1;31m' + "run pipenv install 'moduleName' " + '\x1b[0m')
 
 
 os.environ['FLASK_ENV'] = 'development'  # set flask envoirnment variable
@@ -15,12 +17,124 @@ os.environ['FLASK_ENV'] = 'development'  # set flask envoirnment variable
 app = Flask(__name__)
 portnum = 8080  # custome port to run server on
 
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
 
-@app.route('/')
-@app.route('/index')
-def index():
-    """Return homepage."""
-    render_template('home.html', msg='Hi my name is server')
+
+@app.route('/', methods=['GET'])
+def playlists_index():
+    """ Show all playlists
+    @GET:
+        description:
+        responses:
+            200:
+                description:
+            400:
+                description:
+    """
+    return render_template('playlists_index.html', playlists=playlists.find())
+
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """ Submit a new playlist
+    @POST:
+        description:
+        responses:
+            200:
+                description:
+            400:
+                description:
+    """
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlist_id = playlists.insert_one(playlist).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+
+@app.route('/playlists/new')
+def playlists_new():
+    """ Create a new playlist
+    @POST:
+        description:
+        responses:
+            200:
+                description:
+            400:
+                description:
+    """
+    return render_template('playlists_new.html', playlist={}, title='New Playlist')
+
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """ Show a single playlist
+    @GET:
+        description:
+        responses:
+            200:
+                description:
+            400:
+                description:
+    """
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
+
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """ Submit an edited playlist
+    @POST:
+        description:
+        responses:
+            200:
+                description:
+            400:
+                description:
+    """
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """ Show the edit form for a playlist.
+    @GET:
+        description:
+        responses:
+            200:
+                description:
+            400:
+                description:
+    """
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlists_delete(playlist_id):
+    """ Delete a playlist
+    @GET:
+        description:
+        responses:
+            200:
+                description: 
+            400:
+                description:
+    """
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
 
 
 if __name__ == '__main__':
--- packages/errors.py
+++ packages/errors.py
@@ -4,3 +4,19 @@ import os
 
 class ConnectionError(Exception):
     pass
+
+
+class Version(Exception):
+    def __init__(self, text, version):
+        super().__init__(text)
+        self.version = str(version)
+
+    @ValueError
+    def checkversion(self, version):
+        try:
+            if '3' in sys.version[0]:
+                sys.stdout.write(
+                    '\x1b[1;32m' + '[+] Running Python version: ' + sys.version + '\x1b[0m' + '\n')
+        except:
+            sys.stderr.write(
+                '\x1b[1;31m' + 'Run application with latest version of python' + '\x1b[0m')
